3991484edb0c2b04e2fe203e898d3ad504d62b73
ingwersen-erik <78963102+ingwersen-erik@users.noreply.github.com>
2021-02-23 21:22:18 -0300

    Update underscore-1.3.1.js

--- docs/_build/html/_static/underscore-1.3.1.js
+++ docs/_build/html/_static/underscore-1.3.1.js
@@ -1,3 +1,5 @@
+// file deepcode ignore UncomaparableValues:  File is part of sphinx code
+
 //     Underscore.js 1.3.1
 //     (c) 2009-2012 Jeremy Ashkenas, DocumentCloud Inc.
 //     Underscore is freely distributable under the MIT license.

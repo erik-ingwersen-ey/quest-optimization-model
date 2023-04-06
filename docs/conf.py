bb7dbd782b9b8f99f6028655d2bf418182a79f14
ingwersen-erik <erik.ingwersen@br.ey.com>
2023-04-06 16:05:35 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../optimization'))
 os.system("pip install sphinx-pyreverse")
-
+os.system("pip install pydata-sphinx-theme")
 
 # -- Project information -----------------------------------------------------
 
